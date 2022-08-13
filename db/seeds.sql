6528808ec183c3c080dbd58c291a307b48888c4e
Olivia <ramsfol@gmail.com>
2022-08-13 15:40:22 -0500

    added FK to role_id on employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,3 @@
-INSERT INTO roles (title, salary, department_id)
-VALUES
-('Engineer', 100000, 2),
-('Sales Rep', 85000, 4),
-('Manager', 95000, 3),
-('Customer Service Rep', 70000, 1);
 
 INSERT INTO department (name)
 VALUES 
@@ -11,3 +5,17 @@ VALUES
 ('IT'),
 ('Management'),
 ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Engineer', 100000, 2),
+('Sales Rep', 85000, 4),
+('Manager', 95000, 3),
+('Customer Service Rep', 70000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+('Tim', 'Fake', 1),
+('Gal', 'Mann', 2),
+('Juan', 'Night', 3),
+('Steph', 'Shine', 4);
